7106d82908fc28edc8ef5e91d46912cb3611f5f5
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-03-04 14:40:35 +0200

    components-style-first-commit

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,8 +1,15 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import styles from "./Popup.module.css";
+import { useEffect } from "react";
 
 export const Popup = ({ className, children }) => {
+  useEffect(() => {
+    document.documentElement.classList.add("open-popup");
+    return () => {
+      document.documentElement.classList.remove("open-popup");
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <div className={clsx(styles.card, className)}>{children}</div>
